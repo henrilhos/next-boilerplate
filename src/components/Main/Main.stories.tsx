d560ab2b90e3804e6659153abb4ca5cee70fc8f8
Henrique de Castilhos <henrique@castilhos.dev>
2020-08-18 12:12:03 -0300

    refactor: :art: Use Storybook Essentials

--- src/components/Main/Main.stories.tsx
+++ src/components/Main/Main.stories.tsx
@@ -1,4 +1,3 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/react/types-6-0'
 
 import Main from '.'
@@ -6,15 +5,6 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components!'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
